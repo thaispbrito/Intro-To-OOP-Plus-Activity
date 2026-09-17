d396bfd9e332708ced90d49aeb7599f9faf0d44b
Thais Palma de Brito <britop.tech@gmail.com>
2026-09-17 13:04:30 -0700

    Create Student class

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,17 @@
 # add your Student class here!
+
+class Student:
+    def __init__(self, name, level, classes):
+        self.name = name
+        self.level = level
+        self.classes = classes
+    
+    def add_class(self, class_name):
+        self.classes.append(class_name)
+        return self.classes
+        
+    def get_num_classes(self):
+        return len(self.classes)
+        
+    def summary(self):
+        return f"{self.name} is a {self.level} enrolled in {self.get_num_classes()} classes"
